f3a23729526971128e1f0d4cf1a5211c3d9daf86
harikrish-24 <haripriya.kk2501@gmail.com>
2025-06-22 15:33:55 -0400

    Fix the bugs in palindrome

--- palindrome.js
+++ palindrome.js
@@ -1,12 +1,13 @@
 // FUNCTION IMPLEMENTATION (MULTIPLE BUGS)
 const isPalindrome = function(str) {
-  const noSpaces = str.split(" ").join("");
+  const noSpaces = str.split(" ").join("").toLowerCase();
   const midIndex = Math.floor(noSpaces.length/2);
   const lastIndex = noSpaces.length - 1;
 
   for (let i = 0; i < midIndex; i++) {
-    if (str[i] !== str[lastIndex - i]) return false;
+    if (noSpaces[i] !== noSpaces[lastIndex - i]) return false;
   }
+  return true;
 }
 
 // Assertion Function
